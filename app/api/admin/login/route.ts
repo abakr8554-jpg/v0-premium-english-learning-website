78c80286be7f2e036152475755c5c76b10636af4
abakr8554-jpg <abakr8554@gmail.com>
2026-06-01 16:18:32 +0300

    Update route.ts
    fix

--- app/api/admin/login/route.ts
+++ app/api/admin/login/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 import { cookies } from "next/headers"
-
+export const dynamic = "force-static";
 export async function POST(request: Request) {
   try {
     const { password } = await request.json()
