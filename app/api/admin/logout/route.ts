9dd9f64cc4971235c46c2331b20cddb0c9ce6d68
abakr8554-jpg <abakr8554@gmail.com>
2026-06-01 16:18:16 +0300

    Update route.ts
    fix

--- app/api/admin/logout/route.ts
+++ app/api/admin/logout/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 import { cookies } from "next/headers"
-
+export const dynamic = "force-static";
 export async function POST() {
   const cookieStore = await cookies()
   cookieStore.delete("admin_auth")
